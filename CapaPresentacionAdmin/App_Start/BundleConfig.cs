6f1bb8c905930de1f65264f5a5f7bafba93aaae9
haroldgarcia14 <osmanyg14@gmail.com>
2024-03-04 21:54:31 -0600

    arreglo de bundles y enlaces jquery, datatables, css(un metodo)

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -24,11 +24,11 @@ namespace CapaPresentacionAdmin
 
             //Nuevo bundles
             bundles.Add(new Bundle("~/bundles/complementos").Include(
-                "~/Scripts/scripts.js",
+                "~/Scripts/jquery-3.4.1.min.js",
                 "~/Scripts/fontawesome/all.min.js",
-                "~/Scripts/DataTables/jquery.dataTables.js",
-                "~/Scripts/DataTables/dataTables.responsive.js"
-                ));
+               // "~/Scripts/DataTables/jquery.dataTables.js",
+                //"~/Scripts/DataTables/dataTables.responsive.js",
+                "~/Scripts/scripts.js"));
 
             /*
             //estos bundles no se usaran en este proyecto por ahora
@@ -63,9 +63,9 @@ namespace CapaPresentacionAdmin
 
             //nuevo
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/site.css",
-                "~/Content/DataTables/css/jquery.dataTables.css",
-                "~/Content/DataTables/css/responsive.dataTables.css"
+                "~/Content/site.css"
+                //"~/Content/DataTables/css/jquery.dataTables.css",
+                //"~/Content/DataTables/css/responsive.dataTables.css"
                 ));
         }
     }
